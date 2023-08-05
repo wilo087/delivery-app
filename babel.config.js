752e268d1e84164e3899ba5b5b1db817dec97cbe
Wilowayne De La Cruz G <wilo0087@gmail.com>
2023-08-04 22:04:21 -0400

    Adding config to babel to use TS's custom path

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,19 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-};
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        alias: {
+          '@components': './src/components',
+          '@containers': './src/containers',
+          '@services': './src/services',
+          '@contexts': './src/contexts',
+          '@navigation': './src/navigation',
+          '@types': './src/types/index.ts',
+        },
+      },
+    ],
+  ],
+}
